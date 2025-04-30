252e5ea91d83640d91395f280eb338552b9af40b
kunal-patel07 <kunnalpatel2004@gmail.com>
2025-04-30 13:10:51 +0530

    Delete task1-basic_v1 and Result Calculator folders

--- Result Calculator/src/Store/AppStore.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import Reducers from "./Reducers"
-let AppStore = configureStore({
-reducer :{
-    Reducers:Reducers
-  }
-})
-export default AppStore;
